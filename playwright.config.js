403ce5662a3e5cf914e9da1b678cadaa065f8113
Angelo Herrera <angelogioherrera20@gmail.com>
2026-09-02 00:04:36 +0800

    added actionTimeout

--- playwright.config.js
+++ playwright.config.js
@@ -41,6 +41,7 @@ export default defineConfig({
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		baseURL: process.env.BASE_URL,
 		trace: 'on-first-retry',
+		actionTimeout: 60000,
 	},
 
 	/* Configure projects for major browsers */
